369711e13504ecb878dc2448abd4743b5a3d56e0
John Doe <johndoe@johndoe.com>
2022-01-02 00:12:40 -0500

    add to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChatroomComponent } from "./chatroom/chatroom.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { CreateRoomComponent } from "./create-room/create-room.component";
+import { RoomListComponent } from "./room-list/room-list.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const routes: Routes = [
+  { path: "rooms/:id", component: ChatroomComponent },
+  { path: "rooms/create", component: CreateRoomComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "rooms", component: RoomListComponent },
+  // more routes are added here
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
