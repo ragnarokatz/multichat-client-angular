3c9be71f2d94ce3dca3603ca4a1e075a18920bc7
John Doe <johndoe@johndoe.com>
2022-01-02 00:07:07 -0500

    generate empty components for front end webapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    RoomListComponent,
+    CreateRoomComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
